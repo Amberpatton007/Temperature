5e25d630ff6e079af198c759f2daf5371e0aaf6e
Amber Patton <amberpatton007@Ambers-iMac.lan>
2024-04-10 18:58:18 -0700

    the corrected code that works

--- Scripts/celciusandfarenheit.js
+++ Scripts/celciusandfarenheit.js
@@ -1,18 +1,17 @@
 function convertTemp (){
-    let FarenheitDB = "Farenheit";
-    let CelciusDB = "Celcius";
-
-    var a=FarenheihtDB (CelciusDB - 32) * 5/9;
-    var b=CelciusDB (FarenheitDB * 9/5) + 32;
-
-    let Farenheit = prompt ("Please enter temperature number");
-    let Celcius = prompt ("Please enter either Farenheit or Celcius");
-
-    if(Celcius == FarenheitDB){
-        console.log("Here's the temperature in Farenheit")
-}else if(Farenheit == CelciusDB){
-    console.log("Here's the temperature in Celcius")}
+    let option = prompt("Enter the option Celcius,  or Farenheit");
+    let Temperature = Number(prompt("please enter the temperature"));
+    let result= document.getElementById("result")
 
 
+    if(option = "Celcius"){
+        let f=(Temperature * 9/5) + 32
+        result.innerHTML = (f)
+};
+
+    if(option = "Farenheit"){
+        let c=(Temperature - 32) * 5/9
+        result.innerHTML = (c)
+    }
 }
 
